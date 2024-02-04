458f5f9d17f468e807c571c643e4b8a18e5ff9b3
KokichaKolevTM <86848204+KokichaKolevTM@users.noreply.github.com>
2024-02-04 20:42:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const searchButton = document.querySelector("#searchButton");
+const input = document.querySelector("#inputField");
+const placesList = document.querySelector("#places");
+const modifierBox = document.querySelector("#modifierBox");
+const numberOfResultsText = document.querySelector("#numberOfResults");
+
+searchButton.addEventListener("click", () => {
+    if (placesList.childElementCount !== 0) {
+        Array.from(placesList.childNodes).forEach((e) => e.remove());
+    }
+
+    let checkCondition = "";
+    switch (modifierBox.value) {
+        case "startsWith":
+            checkCondition = ".startsWith(input.value.toLowerCase())";
+            break;
+        case "endsWith":
+            checkCondition = ".endsWith(input.value.toLowerCase())";
+            break;
+        case "contains":
+            checkCondition = ".includes(input.value.toLowerCase())";
+            break;
+        case "match":
+            checkCondition = " === input.value.toLowerCase()";
+            break;
+    }
+
+    let numberOfResults = 0;
+    for (const place of places) {
+        if (eval(`place.name.toLowerCase()${checkCondition}`) === true) {
+            const listEntry = document.createElement("li");
+            listEntry.textContent = `${place.name} (${place.oblast_name})`;
+            placesList.append(listEntry);
+            numberOfResults++;
+        }
+    }
+    numberOfResultsText.textContent = `${numberOfResults} резултата`;
+});
+
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        searchButton.click();
+    }
+});
